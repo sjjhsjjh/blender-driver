534bc639dec05cd3e1b856f061586426042262cc
Jim Hawkins <jhawkins@hands.com>
2017-03-11 17:52:34 +0000

    Moved the text width business from the text application into a new separate module, TextUtilities. Removed all the diagnostic code.

--- applications/text.py
+++ applications/text.py
@@ -31,34 +31,39 @@ import threading
 #
 # Third party modules, in alphabetic order.
 #
-# Blender Game Engine KX_GameObject
-# Import isn't needed because this class gets an object that has been created
-# elsewhere.
-# https://www.blender.org/api/blender_python_api_current/bge.types.KX_GameObject.html
+# Blender library imports, in alphabetic order.
 #
-# Blender Game Engine maths utilities, which can only be imported if running
-# from within the Blender Game Engine.
-# http://www.blender.org/api/blender_python_api_current/mathutils.html
-# This class gets a Vector from the bpy layer, so Vector needn't be imported.
-from mathutils import Matrix
-#
-# Main Blender Python interface.
-# Import isn't needed because the base class keeps a reference to the interface
-# object.
-# import bpy
-#
-# Font Drawing module, used to get text width.
-# https://docs.blender.org/api/blender_python_api_current/blf.html
-#
-# bpy.ops.font.open(filepath="/home/jim/Downloads/fonts/IM Fell English/FeENrm2.ttf")
-# bpy.data.curves['ememem'].font = bpy.data.fonts['IM_FELL_English_Roman']
-#
-import blf
-#
+# These modules can only be imported if running from within Blender.
+try:
+    # Main Blender Python interface.
+    # Import isn't needed because the base class keeps a reference to the
+    # interface object.
+    # import bpy
+    #
+    # Blender Game Engine KX_GameObject
+    # Import isn't needed because this class gets an object that has been
+    # created elsewhere.
+    # https://www.blender.org/api/blender_python_api_current/bge.types.KX_GameObject.html
+    #
+    # Blender Game Engine maths utilities, which can only be imported if running
+    # from within the Blender Game Engine.
+    # http://www.blender.org/api/blender_python_api_current/mathutils.html
+    # This class gets a Vector from the bpy layer, so Vector needn't be
+    # imported.
+    from mathutils import Matrix
+    #
+    # Font Drawing module, used to get text width.
+    # https://docs.blender.org/api/blender_python_api_current/blf.html
+    import blf
+except ImportError as error:
+    print(__doc__)
+    print(error)
 # Local imports.
 #
 # Blender Driver application with threads and locks.
 import blender_driver.application.thread
+#
+import blender_driver.textutils
 
 # Diagnostic print to show when it's imported, if all its own imports run OK.
 print("".join(('Application module "', __name__, '" ')))
@@ -72,28 +77,7 @@ class Application(blender_driver.application.thread.Application):
         'cube': {'subtype':'Cube', 'physicsType':'NO_COLLISION'
                  , 'scale':(0.1, 0.1, 0.1)
                  #, 'scale':(0.15, 0.5, 5.0)
-                 },
-        'em': {'text':"m", 'physicsType':'NO_COLLISION'},
-        'emem': {'text':"mm", 'physicsType':'NO_COLLISION'},
-        'ememem': {'text':"mmm", 'physicsType':'NO_COLLISION'},
-        'en': {'text':"n", 'physicsType':'NO_COLLISION'},
-        'enen': {'text':"nn", 'physicsType':'NO_COLLISION'},
-        'enenen': {'text':"nnn", 'physicsType':'NO_COLLISION'},
-        'emnlem': {'text':"m\nm", 'physicsType':'NO_COLLISION'},
-        'emsmall': {'text':"m", 'physicsType':'NO_COLLISION'
-                    , 'scale':(0.5, 0.5, 0.5)},
-        'emnlemsmall': {'text':"m\nm", 'physicsType':'NO_COLLISION'
-                      , 'scale':(0.5, 0.5, 0.5)},
-        'emwide': {'text':"m", 'physicsType':'NO_COLLISION'
-                    , 'scale':(2.0, 1.0, 1.0)},
-        'ememwide': {'text':"mm", 'physicsType':'NO_COLLISION'
-                    , 'scale':(2.0, 1.0, 1.0)},
-        'spacenl': {'text':" \n", 'physicsType':'NO_COLLISION'},
-        'space': {'text':" ", 'physicsType':'NO_COLLISION'},
-        'zero': {'text':"0", 'physicsType':'NO_COLLISION'
-                 , 'location': (0, -2, 0)},
-        'zerozero': {'text':"00", 'physicsType':'NO_COLLISION'
-                 , 'location': (0, -4, 0)},
+                 }
     }
     
     # Override.
@@ -101,8 +85,12 @@ class Application(blender_driver.application.thread.Application):
         return " ".join((__package__, __name__, str(subroutine)))
 
     def data_initialise(self):
+        self._textUtilities = blender_driver.textutils.TextUtilities(self.bpy)
+        self._textUtilities.data_initialise(self.bpyutils)
         self.bpyutils.delete_except(
-            [self.dataGateway, 'Lamp'] + list(self.templates.keys()) )
+            [self.dataGateway, 'Lamp']
+            + list(self.templates.keys())
+            + self._textUtilities.objectNames)
     
     @property
     def textBoxIndex(self):
@@ -123,72 +111,29 @@ class Application(blender_driver.application.thread.Application):
     def postion_cube(self):
         if self._cube is not None:
             cubePosition = self.textBox.worldPosition.copy()
-            # cubePosition[0] -= (self._cubeDimensions[0]
-            #                     * 0.5
-            #                     * self._cube.worldScale[0]) #+ 0.01
-            # cubePosition[1] += (self._cubeDimensions[1]
-            #                     * 0.5
-            #                     * self._cube.worldScale[1])
-            # cubePosition[2] += ((self.arguments.infoOffset * 0.5)
-            #                     - (self._cubeDimensions[2] * 0.5))
             self._cube.worldPosition = cubePosition
         if self._cube2 is not None:
             cubePosition = self.textBox.worldPosition.copy()
-            cubePosition[1] += self._textDimensions[self.textBoxIndex] * 1.35
+            cubePosition[1] += self._textDimensions[self.textBoxIndex]
             self._cube2.worldPosition = cubePosition
-        
     
     def update_info(self):
-        dimensionsFloat = list(blf.dimensions(0, self.textBox.text))
-
-        # # object_ = self.bpy.data.objects['text']
-        # name = 'temporary'
-        # curve = self.bpy.data.curves.new(name, 'FONT')
-        # curve.align_x = 'CENTER'
-        # curve.align_y = 'CENTER'
-        # curve.body = self.textBox.text
-        # object_ = self.bpy.data.objects.new(name, curve)
-        # # print(object_.dimensions)
-        # scene = self.bpy.data.scenes[0]
-        # # object_.data.body = self.textBox.text
-        # 
-        # scene.objects.link(object_)
-        # scene.update()
-        # # print(object_.dimensions)
-        # dimensionsFloat.extend((object_.dimensions[0], object_.dimensions[1]))
-        # self._textDimensions[self.textBoxIndex] = object_.dimensions[0]
-
-        name = 'ememem'
-        emememObjD = self.bpy.data.objects[name].dimensions[0]
-        emememTxtD = blf.dimensions(0, self.templates[name]['text'])[0]
-        boxTxtD = blf.dimensions(0, self.textBox.text)[0]
-        self._textDimensions[self.textBoxIndex] = (
-            (boxTxtD / emememTxtD) * emememObjD)
-        dimensionsFloat.append(self._textDimensions[self.textBoxIndex])
-
-
-
-        # if self._cube is not None:
-            # worldScale = self._cube.worldScale.copy()
-            # self._cube.worldScale = [0.15
-            #                          , object_.dimensions[0]# * 0.5
-            #                          , object_.dimensions[1]# * 0.5
-            #                          ]
-        # self.bpy.data.objects.remove(object_, True)
-        # self.bpy.data.curves.remove(curve, True)
-        
+        dimensions = list(blf.dimensions(0, self.textBox.text))
+        textWidth = self._textUtilities.text_width(self.textBox.text)
+        self._textDimensions[self.textBoxIndex] = textWidth
+        dimensions.append(textWidth)
         self.postion_cube()
-
-        dimensionsStr = []
-        for dimension in dimensionsFloat:
-            dimensionsStr.append("{:.2f}".format(dimension))
-        self.textInfo.text = "\n".join(dimensionsStr)
+        self.textInfo.text = "\n".join(
+            "{:.2f}".format(dimension) for dimension in dimensions)
 
     # Override.
     def game_initialise(self):
         super().game_initialise()
         self.mainLock.acquire()
         try:
+            self._textUtilities = blender_driver.textutils.TextUtilities(
+                self.bpy)
+            
             self._textBoxIndex = None
             self._cube = None
             self._cube2 = None
@@ -223,7 +168,6 @@ class Application(blender_driver.application.thread.Application):
             if self.arguments.verbose:
                 print(self._name('game_initialise')
                       , self._cubeDimensions, self._cube.worldScale)
-            self.print_dimensions()
 
             self._cube2 = self.gameScene.addObject('cube', self.gameGateway)
 
@@ -232,51 +176,6 @@ class Application(blender_driver.application.thread.Application):
             print("Ctrl-Q to terminate.")
         finally:
             self.mainLock.release()
-    
-    def print_dimensions(self):
-        if self.arguments.verbose:
-            keys = []
-            keys[:] = self.templates.keys()
-            keys.sort()
-            for name in keys:
-                template = self.templates[name]
-                if 'text' in template:
-                    print(name
-                          , self.bpy.data.objects[name].dimensions
-                          , blf.dimensions(0, template['text']))
-            name = 'em'
-            emObjD = self.bpy.data.objects[name].dimensions[0]
-            emTxtD = blf.dimensions(0, self.templates[name]['text'])[0]
-            name = 'emem'
-            ememObjD = self.bpy.data.objects[name].dimensions[0]
-            ememTxtD = blf.dimensions(0, self.templates[name]['text'])[0]
-            name = 'ememem'
-            emememObjD = self.bpy.data.objects[name].dimensions[0]
-            emememTxtD = blf.dimensions(0, self.templates[name]['text'])[0]
-            name = 'en'
-            enObjD = self.bpy.data.objects[name].dimensions[0]
-            enTxtD = blf.dimensions(0, self.templates[name]['text'])[0]
-            name = 'enen'
-            enenObjD = self.bpy.data.objects[name].dimensions[0]
-            enenTxtD = blf.dimensions(0, self.templates[name]['text'])[0]
-            name = 'enenen'
-            enenenObjD = self.bpy.data.objects[name].dimensions[0]
-            enenenTxtD = blf.dimensions(0, self.templates[name]['text'])[0]
-            print(emObjD, emTxtD, ememObjD, ememTxtD)
-            print(ememObjD - emObjD, ememTxtD - emTxtD)
-            print("m", ememObjD - (2.0 * emObjD))
-            print("n", enenObjD - (2.0 * enObjD))
-            # print(ememObjD, ememTxtD, emememObjD, emememTxtD)
-            print(emememObjD - ememObjD, emememTxtD - ememTxtD)
-            print((ememObjD - emObjD) / (ememTxtD - emTxtD))
-            print(2.0 * emObjD, (2.0 * emObjD) - ememObjD)
-            print(3.0 * emObjD, (3.0 * emObjD) - emememObjD)
-            
-            # Divide by this: ememTxtD - emTxtD
-            # Multiply by this: (ememObjD - emObjD)
-            # Add the value for an empty text, given by:
-            # emObjD - (ememObjD - emObjD)
-            
 
     def add_text(self, objectName='text', text=""):
         object_ = self.gameScene.addObject(objectName, self.gameGateway)
@@ -287,6 +186,8 @@ class Application(blender_driver.application.thread.Application):
         return object_
     
     def game_keyboard(self, keyEvents):
+        if not self._textUtilities.initialised:
+            self._textUtilities.game_initialise()
         keyString, ctrl, alt = self.key_events_to_string(keyEvents)
         if self.arguments.verbose:
             print(self._name('game_keyboard'), str(keyEvents)
@@ -302,7 +203,6 @@ class Application(blender_driver.application.thread.Application):
             if status != self.bge.logic.KX_INPUT_JUST_ACTIVATED:
                 continue
             if key == self.bge.events.TABKEY:
-                self.print_dimensions()
                 self.textBoxIndex += 1
                 append = False
             if (key == self.bge.events.BACKSPACEKEY
---
+++ blender_driver/textutils.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+# (c) 2017 Jim Hawkins. MIT licensed, see https://opensource.org/licenses/MIT
+# Part of Blender Driver, see https://github.com/sjjhsjjh/blender-driver
+"""Utility layer on the Blender Python programming interface for text handling.
+This isn't the utilities for the Blender Game Engine.
+
+This module can only be used from within Blender."""
+# Exit if run other than as a module.
+if __name__ == '__main__':
+    print(__doc__)
+    raise SystemExit(1)
+
+# Standard library imports, in alphabetic order, would go here.
+#
+# Blender library imports, in alphabetic order.
+#
+# These modules can only be imported if running from within Blender.
+try:
+    # Font Drawing module, used to get text width.
+    # https://docs.blender.org/api/blender_python_api_current/blf.html
+    #
+    # bpy.ops.font.open(filepath="/home/jim/Downloads/fonts/IM Fell English/FeENrm2.ttf")
+    # bpy.data.curves['ememem'].font = bpy.data.fonts['IM_FELL_English_Roman']
+    #
+    import blf
+except ImportError as error:
+    print(__doc__)
+    print(error)
+#
+# Local imports.
+#
+from . import bpyutils
+
+class TextUtilities(object):
+    _calibratorName = "TextUtilitiesmmmmm"
+    _bpy = None
+    _calibratorRatio = None
+    # _bpyutils = None
+    _objectNames = None
+    
+    @property
+    def initialised(self):
+        return False if self._calibratorRatio is None else True
+    
+    @property
+    def objectNames(self):
+        return self._objectNames
+
+    def data_initialise(self, bpyutils, objectName=None):
+        if objectName is None:
+            objectName = self._calibratorName
+        else:
+            self._calibratorName = objectName
+        bpyutils.set_up_object(
+            objectName, {'text':"mmmmm", 'physicsType':'NO_COLLISION'})
+        self._objectNames = [objectName]
+    
+    def game_initialise(self, objectName=None):
+        if objectName is None:
+            objectName = self._calibratorName
+        else:
+            self._calibratorName = objectName
+        self._objectNames = [objectName]
+        object_ = self._bpy.data.objects[objectName]
+        self._calibratorRatio = (
+            object_.dimensions[0] / blf.dimensions(0, object_.data.body)[0])
+        
+    def text_width(self, text):
+        if self._calibratorRatio is None:
+            return 0.0
+        #
+        # Next line has an unexplained fudge factor: 1.35
+        return blf.dimensions(0, text)[0] * self._calibratorRatio * 1.35
+
+    def __init__(self, bpy):
+        self._bpy = bpy

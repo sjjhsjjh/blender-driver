8dddbb4e0573c008b2e326ea98fd3cfa2982c769
Jim Hawkins <jhawkins@hands.com>
2017-04-02 16:26:50 +0100

    First basic demonstration instructions. Added a demonstration script, and instructions.
    (Previous commit had no message, by mistake.)

---
+++ scripts/marker.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# (c) 2017 Jim Hawkins. MIT licensed, see https://opensource.org/licenses/MIT
+# Part of Blender Driver, see https://github.com/sjjhsjjh/blender-driver
+"""Script to start Blender and Blender Driver, and a specific Blender Driver
+application.
+
+This script is built on the main Blender Driver start script.
+"""
+# Standard library imports, in alphabetic order.
+#
+# Module for file and directory paths.
+# https://docs.python.org/3.5/library/os.path.html
+from os.path import basename, splitext
+#
+# Module for access to the command line.
+# https://docs.python.org/3.5/library/sys.html
+import sys
+#
+# Local imports.
+#
+# Script to create Blender Driver command line.
+from blenderdriver import main
+
+# Set the application module name from the name of this script, without the
+# extension.
+application = splitext(basename(__file__))[0]
+
+main(sys.argv + [
+    '--start'
+    , '--applicationModule', ".".join(('applications', application))
+    , '--'
+    , '--sleep', "0.01"])

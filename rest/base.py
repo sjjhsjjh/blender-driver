d8e19fa3925dd487bcb340623a9bae5f7aaf40e7
Jim Hawkins <jhawkins@hands.com>
2017-04-29 19:18:14 +0100

    WiP commit with new approach working.

--- rest/base.py
+++ rest/base.py
@@ -172,201 +172,61 @@ class RestInterface(object):
                 return list(extension)
     
     def _set_path(self, parent, path, value, enumerator):
-        self.verbosely(__name__, 'set_path()', parent, path, value)
-        point0 = None
-        point1 = parent
-        return_ = None
-        type = None
-        lastLeg = None
-        while True:
-            try:
-                index, (leg, legStr) = next(enumerator)
-                lastLeg = leg
-            except StopIteration:
-                break
+        self.verbosely(__name__, '_set_path()', parent, path, value)
 
-            point0 = point1
-            point1, numeric, type = RestInterface.get_point(point0, leg)
-            if numeric and isinstance(point0, str):
-                # Sorry, have to force descent into a string to fail.
-                point1 = None
-            self.verbosely(
-                "{:2d} {}\n  {}\n  {}\n  {} {}".format(
-                    index, legStr, point0, point1 , str(numeric), str(type)))
+        try:
+            index, (leg, legStr) = next(enumerator)
+        except StopIteration:
+            return value
+        
+        wasTuple = isinstance(parent, tuple)
 
-            if type is None or point1 is None:
-                point0 = self.make_point(path, index, point0)
-                point1, numeric, type = RestInterface.get_point(point0, leg)
+        point, numeric, type = RestInterface.get_point(parent, leg)
+        if numeric and isinstance(parent, str):
+            # Sorry, have to force descent into a string to fail.
+            point = None
+        
+        self.verbosely(__name__,
+            "{:2d} {}\n  {}\n  {}\n  {} {}".format(
+                index, legStr, parent, point, str(numeric), str(type)))
 
-            if type is None:
-                raise AssertionError("".join((
-                    "type was None twice at ", str(point0)
-                    ," path:", str(path), " index:", index
-                    , " leg:", legStr, ".")))
-            
-            if return_ is None:
-                return_ = point0
-                self.verbosely(__name__, 'set_path set return', return_)
+        if type is None or point is None:
+            parent = self.make_point(path, index, parent)
+            self.verbosely(__name__, '_set_path made point', parent)
+            point, numeric, type = RestInterface.get_point(parent, leg)
 
-            if point1 is None:
-                # Nowhere to descend.
-                #
-                # Generate a new tree, recursively.
-                value = self._set_path(None, path, value, enumerator)
-                #
-                # Break out of the loop so that the value setting code inserts
-                # the new tree.
-                break
+        if type is None:
+            raise AssertionError("".join((
+                "type was None twice at ", str(parent)
+                ," path:", str(path), " index:", str(index)
+                , " leg:", legStr, ".")))
 
-        self.verbosely(__name__, 'set_path loop finished'
-                       , lastLeg, point0, return_)
-        if lastLeg is None:
-            return_ = value
-        elif type == RestInterface.PointType.ATTR:
-            setattr(point0, lastLeg, value)
+        value = self._set_path(point, path, value, enumerator)
+
+        # ToDo: optimise to set only if new value is different to current value.
+        if type == RestInterface.PointType.ATTR:
+            setattr(parent, leg, value)
         else:
             # leg could be numeric or string; parent could be list or dict ...
             #
             # ... or tuple. If it's a tuple then make it into a list, and
-            # therefore mutable, first. Also, if the tuple is what we were going
-            # to return, change that too.
-            wasReturn = (point0 is return_)
-            if isinstance(point0, tuple):
-                point0 = list(point0)
-                if wasReturn:
-                    return_ = point0
-                    self.verbosely(__name__, 'set_path set wasReturn', return_)
-            point0[lastLeg] = value
+            # therefore mutable, first.
+            if isinstance(parent, tuple):
+                parent = list(parent)
+            parent[leg] = value
 
-        if isinstance(parent, tuple) and isinstance(return_, list):
-            return_ = tuple(return_)
-        return return_
+        if wasTuple and isinstance(parent, list):
+            parent = tuple(parent)
+        
+        return parent
     
     def set_path(self, parent, path, value):
         return self._set_path(
             parent, path, value, enumerate(RestInterface.pathify(path)))
 
     def rest_put(self, value, path=None):
-        self.verbosely('rest_put', value, path)
-
-        self.principal = self.create_tree(self.principal, path, value)
-
-        parent = None
-        leg = None
-        point = self.principal
-        type = None
-        for index, leg in enumerate(paths(path)):
-            legStr = leg.join(('"', '"')) if isinstance(leg, str) else str(leg)
-            self.verbosely(
-                "{:2d} {}\n  {}\n  {}".format(index, legStr, parent, point))
-
-
-            # It's OK for parent to be None when index is zero.
-            parent = point
-
-
-            point, numeric, type = RestInterface.get_point(parent, leg)
-            
-            if type is None:
-                value = self.create_tree(value, path, index, numeric)
-                self.verbosely("Tree value", value)
-                # if index == 0:
-                #     self.principal = tree
-                #     # if tree is None:
-                #     #     if numeric:
-                #     #         self.verbosely('Inserting list.')
-                #     #         self._list = point
-                #     #         parent = self._list
-                #     #     else:
-                #     #         self.verbosely('Inserting dictionary.')
-                #     #         self._dict = point
-                #     #         parent = self._dict
-                #     # else:
-                #     #     self.principal = point
-                #     #     parent = self.principal
-                # else:
-                #     # leg is numeric or string.
-                #     parent[leg] = point
-                break
- 
-                # point, numeric, type = RestInterface.get_point(parent, leg)
-
-            # if type is None:
-            #     raise AssertionError("type was None twice.")
-            
-            if point is None:
-                raise NotImplementedError()
-                if type == RestInterface.PointType.LIST:
-                    self.verbosely('Extending list.')
-                    parent.extend([None] * ((leg - len(parent)) + 1))
-                else:
-                    raise NotImplementedError()
-                # elif type == RestInterface.PointType.DICTIONARY:
-                    # self.verbosely('Extending dictionary.')
-                    # parent[leg] = None
-                point, numeric, type = RestInterface.get_point(parent, leg)
-
-            if type is None:
-                raise AssertionError("type was None after point was None.")
-            
-            
-
-            
-            # If it doesn't, create a suitable point that can contain leg.
-            # Default suitable point is determined by the type of leg:
-            # -   dict if string.
-            # -   list if numeric.
-            # Have an overridable function that returns the new point.
-            # Maybe make RestInterface override all the __getitem__ family so
-            # that it can present as an array or dictionary.
-            
-            
-            # if isinstance(leg, str):
-            #     if restDescent:
-            #         if point.principal is None:
-            #             if point._dict is None:
-            #                 self.verbosely('Adding dictionary', id(point))
-            #                 point._dict = {}
-            #             if leg not in point._dict:
-            #                 point._dict[leg] = RestInterface()
-            #             point = point._dict[leg]
-            #         else:
-            #             restDescent = False
-            #             parent = point.principal
-            #             point, setattr_ = RestInterface.get_key_attr(
-            #                 parent, leg)
-            #     else:
-            #         point, setattr_ = RestInterface.get_key_attr(point, leg)
-            # else:
-            #     # Assume integer.
-            #     setattr_ = False
-            #     if restDescent:
-            #         if point.principal is None:
-            #             if point._list is None:
-            #                 self.verbosely('Adding list', id(point))
-            #                 point._list = []
-            #             if len(point._list) <= leg:
-            #                 self.verbosely('Extending list', id(point))
-            #                 point._list.extend([None] * (
-            #                     (leg - len(point._list)) + 1))
-            #             if point._list[leg] is None:
-            #                 point._list[leg] = RestInterface()
-            #             point = point._list[leg]
-            #         else:
-            #             # Assume principal is a list and descend into it.
-            #             restDescent = False
-            #             parent = point.principal
-            #             point = point.principal[leg]
-            #     else:
-            #         point = point[leg]
-
-        if parent is None:
-            self.principal = value
-        elif type == RestInterface.PointType.ATTR:
-            setattr(parent, leg, value)
-        else:
-            # leg could be numeric or string; parent could be list or dict.
-            parent[leg] = value
+        self.verbosely(__name__, 'rest_put', value, path)
+        self.principal = self.set_path(self.principal, path, value)
 
     def rest_get(self, path=None):
         return RestInterface.get_path(self.principal, path)
@@ -376,11 +236,7 @@ class RestInterface(object):
         self._principal = None
 
     # def __str__(self):
-    #     return str(self.restPrincipal)
-
-# RestInterface with dictionary and array in each node, or RestDictionary and
-# RestArray as subclasses? Could even have RestDictionary and RestArray as
-# principals.
+    #     return str(self.principal)
 
 
 class RestBGEObject(RestInterface):
@@ -395,13 +251,6 @@ class RestBGEObject(RestInterface):
 
 # Do:
 #
-# -   Test module in the same directory.
-# -   Principal objects can't be hierarchical. To create hierarchies with types
-#     and property names, create a RestInterface subclass. Although, it would be
-#     possible to descend by getattr(). 
-# -   Test: Create a RestInterface and attach another object as principal.
-# -   Test: Build an array, by setting [0] as path.
-# -   Double traversal by setting restParent when creating a RestInterface.
 # -   restGetCache that contains only data that has been accessed through the
 #     RestInterface. But have an API to load the cache maybe.
 # -   BGE interface will be like: create a KXGameObject, create a wrapper around

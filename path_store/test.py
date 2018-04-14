b284e6e80ef7f45e3cd2dffe2e6deafa75994ace
Jim Hawkins <jhawkins@hands.com>
2018-04-14 16:33:56 +0100

    Added back the ability to run a specified set of tests, which had been removed by accident. Optimised documentation URLs.

--- path_store/test.py
+++ path_store/test.py
@@ -5,16 +5,20 @@
 
 # Standard library imports, in alphabetic order.
 #
+# Module for command line switches.
+# https://docs.python.org/3/library/argparse.html
+import argparse
+#
 # Module for building paths.
-# https://docs.python.org/3.5/library/os.path.html
+# https://docs.python.org/3/library/os.path.html
 import os.path
 #
 # Module for extending the search path.
-# https://docs.python.org/3.5/library/sys.html
+# https://docs.python.org/3/library/sys.html
 import sys
 #
 # Unit test module.
-# https://docs.python.org/3.5/library/unittest.html
+# https://docs.python.org/3/library/unittest.html
 import unittest
 #
 # Local imports.
@@ -27,12 +31,36 @@ sys.path.append(os.path.abspath(os.path.join(
 # Local module for setting up Python logging.
 from blender_driver.loggingutils import initialise_logging
 
-if __name__ == '__main__':
-    # The next line is carried over from unittest.main().
-    verboseMode = ('-v' in sys.argv) or ('--verbose' in sys.argv)
-    initialise_logging(verboseMode)
-    suites = unittest.defaultTestLoader.discover(
+def discoverTests(specification):
+    discoveredSuites = unittest.defaultTestLoader.discover(
         os.path.join('path_store', 'test'), "*.py")
+    if specification is None or len(specification) <= 0:
+        return discoveredSuites
+
+    # Couldn't see any way to remove tests from a TestSuite. The following code
+    # creates a new empty suite and then adds only the tests that match the
+    # specification.
+    suites = unittest.TestSuite()
+    for suite in discoveredSuites:
+        for test in suite:
+            for case in test:
+                className = case.__class__.__name__
+                if className in specification:
+                    suites.addTest(case)
+    return suites
+
+if __name__ == '__main__':
+    argumentParser = argparse.ArgumentParser()
+    argumentParser.add_argument(
+        '-v', '--verbose', action='store_true', help="Verbose output.")
+    argumentParser.add_argument(
+        'tests', nargs='*', type=str, help=
+        "Tests to run. Default is to run all.")
+    arguments = argumentParser.parse_args()
+
+    initialise_logging(arguments.verbose)
+        
     results = unittest.TextTestRunner(
-        verbosity=(2 if verboseMode else 1)).run(suites)
+        verbosity=(2 if arguments.verbose else 1)
+        ).run(discoverTests(arguments.tests))
     sys.exit(len(results.errors) + len(results.failures))

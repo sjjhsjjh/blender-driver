c6f98f1975de7195356f99e544eaf04ca84576a6
Jim Hawkins <jhawkins@hands.com>
2018-05-07 12:36:05 +0100

    Added summary function. Added support for None in times list. Added basic test.

--- diagnostic/analysis.py
+++ diagnostic/analysis.py
@@ -5,10 +5,6 @@
 
 This module contains analytical utilities that are used in some diagnostic and
 demonstration Blender Driver applications."""
-# Exit if run other than as a module.
-if __name__ == '__main__':
-    print(__doc__)
-    raise SystemExit(1)
 
 # Standard library imports, in alphabetic order.
 #
@@ -16,23 +12,52 @@ if __name__ == '__main__':
 # https://docs.python.org/3/library/math.html
 from math import log10
 
-def timing_analysis(times):
+def timing_analysis(times, timePrecision=4):
     analyses = []
     indexWidth = int(log10(len(times) - 1)) + 1
-    timePrecision = 4
-    timeWidth = int(log10(times[-1])) + 2 + timePrecision
-    for index, tickTime in enumerate(times):
-        base = '{:{indexWidth}d} {:{timeWidth}.{timePrecision}f}'.format(
-            index, tickTime, indexWidth=indexWidth, timeWidth=timeWidth
-            , timePrecision=timePrecision)
+    timeWidth = None
+    for time in times:
+        if time is None:
+            continue
+        timeStr = '{:.{timePrecision}f}'.format(
+            time, timePrecision=timePrecision)
+        if timeWidth is None or len(timeStr) > timeWidth:
+            timeWidth = len(timeStr)
+    lastTime = None
+    for index, time in enumerate(times):
+        indexStr = '{:{indexWidth}d} '.format(index, indexWidth=indexWidth)
+        timeStr = 'None'
         analysis = ""
-        if index > 0:
-            elapsed = tickTime - times[index - 1]
-            analysis = ' {:.{timePrecision}f} 1/{:.0f}'.format(
-                elapsed, 0.0 if elapsed <= 0 else 1.0 / elapsed
-                , timePrecision=timePrecision)
-        analyses.append(''.join((base, analysis)))
+        if time is not None:
+            timeStr = '{:{timeWidth}.{timePrecision}f}'.format(
+                time, timeWidth=timeWidth, timePrecision=timePrecision)
+            if lastTime is not None:
+                elapsed = time - lastTime
+                analysis = ' {:.{timePrecision}f} 1/{:.0f}'.format(
+                    elapsed, 0.0 if elapsed <= 0 else 1.0 / elapsed
+                    , timePrecision=timePrecision)
+            lastTime = time
+        analyses.append(''.join((indexStr, timeStr, analysis)))
     return analyses
 
 def timing_analysis_dump(times):
     return "\n".join(timing_analysis(times))
+
+def timing_summary(times, totalLabel="total", otherLabel="run"
+                   , noneLabel="skipped"
+                   ):
+    nones = times.count(None)
+    others = len(times) - nones
+    return '{}:{:d} {}:{:d}({:.0%}) {}:{:d}({:.0%})'.format(
+        totalLabel, len(times)
+        , otherLabel, others, float(others) / float(len(times))
+        , noneLabel, nones, float(nones) / float(len(times)))
+
+if __name__ == '__main__':
+    print(__doc__)
+    times = [0.0, 1.0, None, 2.5, 2.55]
+    print("\nTest", times)
+    print(timing_analysis_dump(times))
+    print(timing_summary(times))
+
+    raise SystemExit(1)

914c1b49e1f12966af7a8147521d3fce97a96058
Jim Hawkins <jhawkins@hands.com>
2018-05-07 20:45:54 +0100

    Added fall analysis helper.

--- diagnostic/analysis.py
+++ diagnostic/analysis.py
@@ -12,17 +12,13 @@ demonstration Blender Driver applications."""
 # https://docs.python.org/3/library/math.html
 from math import log10
 
+def timing_analysis_dump(times):
+    return "\n".join(timing_analysis(times))
+
 def timing_analysis(times, timePrecision=4):
     analyses = []
-    indexWidth = int(log10(len(times) - 1)) + 1
-    timeWidth = None
-    for time in times:
-        if time is None:
-            continue
-        timeStr = '{:.{timePrecision}f}'.format(
-            time, timePrecision=timePrecision)
-        if timeWidth is None or len(timeStr) > timeWidth:
-            timeWidth = len(timeStr)
+    indexWidth = index_width(times)
+    timeWidth = field_width(times, precision=timePrecision)
     lastTime = None
     for index, time in enumerate(times):
         indexStr = '{:{indexWidth}d} '.format(index, indexWidth=indexWidth)
@@ -40,8 +36,19 @@ def timing_analysis(times, timePrecision=4):
         analyses.append(''.join((indexStr, timeStr, analysis)))
     return analyses
 
-def timing_analysis_dump(times):
-    return "\n".join(timing_analysis(times))
+def index_width(lenable):
+    return len("{:d}".format(len(lenable)- 1))
+
+def field_width(values, precision=4, type="f"):
+    width = None
+    for value in values:
+        if value is None:
+            continue
+        str = '{:.{precision}{type}}'.format(
+            value, precision=precision, type=type)
+        if width is None or len(str) > width:
+            width = len(str)
+    return width
 
 def timing_summary(times, totalLabel="total", otherLabel="run"
                    , noneLabel="skipped"
@@ -53,11 +60,30 @@ def timing_summary(times, totalLabel="total", otherLabel="run"
         , otherLabel, others, float(others) / float(len(times))
         , noneLabel, nones, float(nones) / float(len(times)))
 
+def fall_analysis(positionsTimes):
+    analyses = []
+    lastPosition = None
+    falls = 0
+    indexWidth = index_width(positionsTimes)
+    for index, positionTime in enumerate(positionsTimes):
+        position, time = positionTime
+        if lastPosition == position:
+            fall = " ="
+        else:
+            fall = ""
+            falls += 1
+        lastPosition = position
+            
+        analyses.append('{:{indexWidth}d} {:.2f} {:.4f}{}'.format(
+            index, position, time, fall, indexWidth=indexWidth))
+    return len(analyses) - falls, "\n".join(analyses)
+
 if __name__ == '__main__':
     print(__doc__)
-    times = [0.0, 1.0, None, 2.5, 2.55]
+    times = [0.0, 1.0, None, 2.5, 2.55, 10.0, 10.1, 10.3, 10.5, 10.7, 10.8]
     print("\nTest", times)
     print(timing_analysis_dump(times))
     print(timing_summary(times))
+    print(timing_analysis_dump([]))
 
     raise SystemExit(1)

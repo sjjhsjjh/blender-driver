db6cbfc71baba6d544249fec635e2a8d316f9396
Jim Hawkins <jhawkins@hands.com>
2018-06-10 15:24:37 +0100

    Added fetch of the camera subject.

--- user_interface/diagnostic/index.js
+++ user_interface/diagnostic/index.js
@@ -1,34 +1,52 @@
-class Main {
+class UserInterface {
     createNode(tag, text) {
         var element = document.createElement(tag);
         if ( text !== null ) {
-            element.appendChild( document.createTextNode(text) );
+            element.appendChild(document.createTextNode(text));
         }
         return element;
     }
     
     appendNode(tag, text, parent) {
-        return parent.appendChild( this.createNode(tag, text) );
+        return parent.appendChild(this.createNode(tag, text));
     }
 
     constructor(userInterfaceID) {
         this.userInterface = document.getElementById(userInterfaceID);
     }
     
-    main() {
-        
-        fetch('api/root/camera')
+    fetch_api(...path) {
+        return fetch(['api', ...path].join('/'));
+    }
+    
+    fetch_camera() {
+        this.fetch_api('root', 'camera')
         .then(response => response.json())
         .then(response => {
-            console.log(response);
+            console.log('camera', response);
             this.appendNode('pre',
                             JSON.stringify(response, null, "  "),
                             this.userInterface);
+            if (response.subjectPath) {
+                this.fetch_api(...response.subjectPath)
+                .then(response2 => response2.json())
+                .then(response2 => {
+                    console.log('subject', response2);
+                });
+            }
         });
+    }
+    
+    show() {
+        this.button = this.appendNode('button', "Fetch camera", this.userInterface);
+        this.button.setAttribute('type', 'button');
+        this.button.onclick = this.fetch_camera.bind(this);
+        
+        return this;
     }
 }
 
 function main(userInterfaceID) {
-    let mainMain = new Main(userInterfaceID);
-    return mainMain.main();
+    const userInterface = new UserInterface(userInterfaceID);
+    return userInterface.show();
 }

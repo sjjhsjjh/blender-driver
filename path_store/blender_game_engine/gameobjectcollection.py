4119b957be12cf2f62e23d8e41a3b5d2f4340c00
Jim Hawkins <jhawkins@hands.com>
2018-08-30 21:53:57 +0100

    Added endObject() when changing the collection item.

--- path_store/blender_game_engine/gameobjectcollection.py
+++ path_store/blender_game_engine/gameobjectcollection.py
@@ -14,6 +14,8 @@ if __name__ == '__main__':
 # Module that facilitates container subclasses.
 # https://docs.python.org/3/library/collections.html#collections.UserList
 import collections
+# Data model reference documentation is also useful:
+# https://docs.python.org/3/reference/datamodel.html#emulating-container-types
 
 class GameObjectList(collections.UserList):
     def __delitem__(self, specifier):
@@ -27,6 +29,12 @@ class GameObjectList(collections.UserList):
                 list_[specifier].endObject()
     
         list.__delitem__(list_, specifier)
+    
+    def __setitem__(self, index, value):
+        item = self.data[index]
+        if item is not value and item is not None:
+            item.endObject()
+        list.__setitem__(self.data, index, value)
 
 class GameObjectDict(collections.UserDict):
     def __delitem__(self, key):
@@ -34,3 +42,9 @@ class GameObjectDict(collections.UserDict):
         if object_ is not None:
             object_.endObject()
         dict.__delitem__(self.data, key)
+    
+    def __setitem__(self, key, value):
+        item = self.data[key]
+        if item is not value and item is not None:
+            item.endObject()
+        dict.__setitem__(self.data, key, value)
